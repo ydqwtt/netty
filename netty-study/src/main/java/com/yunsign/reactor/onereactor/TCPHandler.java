c92ec613e1d91e9e2424066c0dd4dc275f03f209
wei.yang.nj <wei.yang.nj@99bill.com>
2019-09-05 20:03:54 +0800

    netty

--- netty-study/src/main/java/com/yunsign/reactor/onereactor/TCPHandler.java
+++ netty-study/src/main/java/com/yunsign/reactor/onereactor/TCPHandler.java
@@ -23,7 +23,8 @@
         }  
       
         @Override  
-        public void run() {  
+        public void run() {
+            System.out.println("run --->state="+state);
             try {  
                 if (state == 0)  
                     read(); // 讀取網絡數據  
@@ -45,7 +46,8 @@
             }  
         }  
       
-        private synchronized void read() throws IOException {  
+        private synchronized void read() throws IOException {
+            System.out.println("read()");
             // non-blocking下不可用Readers，因為Readers不支援non-blocking  
             byte[] arr = new byte[1024];  
             ByteBuffer buf = ByteBuffer.wrap(arr);  
@@ -64,17 +66,22 @@
                         + " > " + str);  
                 state = 1; // 改變狀態  
                 sk.interestOps(SelectionKey.OP_WRITE); // 通過key改變通道註冊的事件  
-                sk.selector().wakeup(); // 使一個阻塞住的selector操作立即返回  
+                sk.selector().wakeup(); // 使一個阻塞住的selector操作立即返回
+                try {
+                    Thread.sleep(10000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }  
         }  
       
         private void send() throws IOException  {  
             // get message from message queue  
-              
+            System.out.println("send()");
             String str = "Your message has sent to "  
                     + sc.socket().getLocalSocketAddress().toString() + "\r\n";  
             ByteBuffer buf = ByteBuffer.wrap(str.getBytes()); // wrap自動把buf的position設為0，所以不需要再flip()  
-      
+
             while (buf.hasRemaining()) {  
                 sc.write(buf); // 回傳給client回應字符串，發送buf的position位置 到limit位置為止之間的內容  
             }  
